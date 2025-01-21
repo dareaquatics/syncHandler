1fcb481d03f3f9afa061198df16cfef02657f420
Ryan Lu <164600355+luryann@users.noreply.github.com>
2025-01-20 22:23:18 -0800

    add constants file to resolve redeclared variables

---
+++ constants.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+    githubRepo = "https://github.com/dareaquatics/dare-website"
+    repoName   = "dare-website"
+    startMarker = "<!-- START UNDER HERE -->"
+    endMarker   = "<!-- END AUTOMATION SCRIPT -->"
+)
