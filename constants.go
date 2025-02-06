a568857298dd5bef8b9ee8b421fcaea6aa8705fb
Ryan Lu <164600355+luryann@users.noreply.github.com>
2025-02-06 13:53:04 -0800

    wtf is this file

--- constants.go
+++
@@ -1,8 +0,0 @@
-package main
-
-const (
-    githubRepo = "https://github.com/dareaquatics/dare-website"
-    repoName   = "dare-website"
-    startMarker = "<!-- START UNDER HERE -->"
-    endMarker   = "<!-- END AUTOMATION SCRIPT -->"
-)
